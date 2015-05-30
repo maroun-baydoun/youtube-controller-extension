709c97c7872f80ed0b85974a58d4769b80dafc90
Maroun Baydoun <maroun.baydoun@gmail.com>
2015-05-30 16:13:32 +0900

    Updated the style

--- loader.js
+++ loader.js
@@ -2,21 +2,21 @@ window.addEventListener('DOMContentLoaded', function () {
     chrome.tabs.query({
         url: "https://www.youtube.com/watch*"
     }, function (tabs) {
-          
-        var videList = document.getElementById('video-list');
+
+        var videoList = document.getElementById('video-list');
         tabs.forEach(function (tab) {
             var videoListItem = document.createElement("li"),
+                videoListItemText = document.createElement("span"),
                 videoControl = document.createElement("button");
-            
+
             videoControl.classList.add("videoControlButton");
             videoControl.dataset.tabId = tab.id;
             videoControl.appendChild(document.createTextNode("Play/Pause"));
             videoControl.addEventListener("click",videoControlClicked);
-            
-            videoListItem.appendChild(document.createTextNode(tab.title));
-            
+            videoListItemText.appendChild(document.createTextNode(tab.title));
+            videoListItem.appendChild(videoListItemText);
             videoListItem.appendChild(videoControl);
-            videList.appendChild(videoListItem);
+            videoList.appendChild(videoListItem);
         });
 
     });
@@ -28,5 +28,5 @@ function videoControlClicked() {
                 code: 'var playButton = document.querySelectorAll(".ytp-button-play")[0];'+
                       'var pauseButton =   document.querySelectorAll(".ytp-button-pause")[0];'+
                       'if(playButton){playButton.click();}else if(pauseButton){pauseButton.click();}'
-            }, function(a){console.log(a);});  
+            }, function(a){console.log(a);});
 }
