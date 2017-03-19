e65c8746985207ae0b9a0aa223127d9671330606
Maroun Baydoun <maroun.baydoun@gmail.com>
2017-03-19 16:56:52 +0900

    Improve code format

--- util.js
+++ util.js
@@ -1,44 +1,45 @@
+"use strict";
 var Util = (function(chrome) {
-  return {
-    queryTabs: function(callback) {
-      chrome.tabs.query({
-        url: "*://*.youtube.com/watch*"
-      }, function(tabs) {
-        if (callback) {
-          callback(tabs);
+    return {
+        queryTabs: function(callback) {
+            chrome.tabs.query({
+                url: "*://*.youtube.com/watch*"
+            }, function(tabs) {
+                if (callback) {
+                    callback(tabs);
+                }
+            });
+        },
+        toggleVideo: function(tabId, callback) {
+            chrome.tabs.executeScript(tabId, {
+                code: 'var video = document.getElementsByTagName("video")[0];' +
+                    'if (video.paused){video.play();} else {video.pause();}' +
+                    'video.paused;'
+            }, function(result) {
+                if (callback) {
+                    callback(result[0]);
+                }
+            });
+        },
+        toggleTab: function(tabId, selected) {
+            chrome.tabs.update(tabId, {
+                selected: selected
+            });
+        },
+        videoPaused: function(tabId, callback) {
+            chrome.tabs.executeScript(tabId, {
+                code: 'var video = document.getElementsByTagName("video")[0];' +
+                    'video.paused;'
+            }, function(result) {
+                var paused = true;
+                if (result && result.length > 0) {
+                    paused = result[0];
+                }
+                if (callback) {
+                    callback(paused);
+                }
+            });
         }
-      });
-    },
-    toggleVideo: function(tabId, callback) {
-      chrome.tabs.executeScript(tabId, {
-        code: 'var video = document.getElementsByTagName("video")[0];' +
-          'if (video.paused){video.play();} else {video.pause();}' +
-          'video.paused;'
-      }, function(result) {
-        if (callback) {
-          callback(result[0]);
-        }
-      });
-    },
-    toggleTab: function(tabId, selected) {
-      chrome.tabs.update(tabId, {
-        selected: selected
-      });
-    },
-    videoPaused: function(tabId, callback) {
-      chrome.tabs.executeScript(tabId, {
-        code: 'var video = document.getElementsByTagName("video")[0];' +
-          'video.paused;'
-      }, function(result) {
-        var paused = true;
-        if (result && result.length > 0) {
-          paused = result[0];
-        }
-        if (callback) {
-          callback(paused);
-        }
-      });
-    }
 
-  }
+    }
 })(chrome)
