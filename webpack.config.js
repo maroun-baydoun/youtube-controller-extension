339744ddfbd3623f911c5c43dff8eb6e73dffedd
Maroun Baydoun <maroun.baydoun@gmail.com>
2020-09-02 00:08:44 +0200

    Add publish script

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 const variables = require('./src/variables');
@@ -64,6 +65,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: isDevelopment ? '[name].css' : '[name].[hash].css',
             chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-        })
+        }),
+        new CleanWebpackPlugin()
     ]
 };
