5d1136a749d062a8ab9bd2441d22c5f4bccca257
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-02-25 00:33:33 +0100

    Add page content

--- src/variables.js
+++ src/variables.js
@@ -1,10 +1,11 @@
 module.exports = {
-  title: "",
-  description: "",
-  heading1: "",
-  heading2: "",
-  githubLink: "",
+  title: "Youtube™ Controller",
+  description:
+    "A Chrome extension that controls Youtube™ videos from any other web page you are visiting, or even when Chrome is in the background.",
+  heading1: "Youtube™ Controller",
+  heading2: "Control Youtube™ videos from any web page",
+  githubLink: "https://github.com/maroun-baydoun/youtube-controller-extension",
   npmLink: "",
-  subPath: "",
-  linkHome: false,
+  subPath: "youtube-controller-extension/",
+  linkHome: true,
 };
